18989d245a902c13bcc3ccb21928b80e30b76786
Alexander Leris <alexander.leris@gmail.com>
2020-03-22 19:10:35 +1100

    Now correctly reads in booleans from command line

--- main.py
+++ main.py
@@ -13,12 +13,18 @@ def main(fname, invert=True):
         print()
         print(b)
 
+def str2bool(string):
+    """Take a string and return if it represents True or False. Errors are returned False"""
+    if string == "True": return True
+    elif string == "False": return False
+    return False
+
 if __name__ == "__main__":
     from sys import argv
     # 2 args: just the filename, 3 args: also invert flag. Need to have a str->bool conversion here
     if len(argv) == 2:
         main(argv[1])
     elif len(argv) == 3:
-        main(argv[1], argv[2])
+        main(argv[1], str2bool(argv[2]))
     else:
         print("usage: python3 script filename [invert]\ninvert is True or False (default True)")
